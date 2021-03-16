2c1159262ce62f63858e5e281258031186897690
Palaman1 <palaman12345@gmail.com>
2021-03-16 18:14:22 +0200

    added shop content

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,32 @@
 $(function(){
 
+  $('.star').rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
+  $('.rated__star').rateYo({
+    starWidth: "20px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.user-nav__link__btn').on('click', function (){
     $('.user-nav__menu').toggleClass('user-nav__menu--active');
   });
